6a4b7b8de5dfc73cd6650365a47e61cb1a49efe6
saogamer622-arch <saogamer622@gmail.com>
2026-01-07 09:29:00 +0700

    OOP_Test_ForLoop

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class NumberCalculator
+{
+    public void ShowAndSum(int n)
+    {
+        int sum = 0;
+
+        Console.Write("แสดงตัวเลข: ");
+        for (int i = 1; i <= n; i++)
+        {
+            Console.Write(i + " ");
+            sum += i;
+        }
+
+        Console.WriteLine();
+        Console.WriteLine("ผลรวม = " + sum);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("ป้อนตัวเลข: ");
+        int n = int.Parse(Console.ReadLine());
+
+        NumberCalculator cal = new NumberCalculator();
+        cal.ShowAndSum(n);
+    }
+}
